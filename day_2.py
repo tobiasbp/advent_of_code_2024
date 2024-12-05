78c0fcb4a6e1135d836449c3ab2612bad5600d4c
tobiasbp <tobiasbp@gmail.com>
2024-12-05 22:56:00 +0100

    Add day 2,2

--- day_2.py
+++ day_2.py
@@ -3,14 +3,12 @@
 from pathlib import Path
 
 # Read the data
-data = Path("data/day_2_1_example.txt").read_text().splitlines()
-# data = Path("data/day_2_1.txt").read_text().splitlines()
+data = Path("data/day_2_1.txt").read_text().splitlines()
 
 
 class Report:
     def __init__(self, levels: list[int]) -> None:
-        # self.levels = levels
-        self._is_safe = self._is_ascending(levels) or self._is_descending(levels)
+        self.levels = levels
 
     def _is_ascending(self, r: list[int]) -> bool:
         for i in range(len(r) - 1):
@@ -23,25 +21,44 @@ class Report:
         r.reverse()
         return self._is_ascending(r)
 
-    @property
-    def is_safe(self) -> bool:
-        return self._is_safe
+    def is_safe(self, dampner_enabled=False) -> bool:
+        if not dampner_enabled:
+            return self._is_ascending(self.levels) or self._is_descending(self.levels)
 
+        # Evaluate variations of the levels with one entry removed
+        for level_index_to_remove in range(len(self.levels)):
+            levels = [
+                l for i, l in enumerate(self.levels) if i != level_index_to_remove
+            ]
+            if self._is_ascending(levels) or self._is_descending(levels):
+                return True
 
-reports = []
+        return False
+
 
 # Parse reports
+reports: list[Report] = []
 for line in data:
     reports.append(Report(list(int(n) for n in line.split())))
 
 print("No of reports:", len(reports))
 
+
 # Problem 1
 no_of_safe_reports = 0
 for r in reports:
-    if r.is_safe:
+    if r.is_safe(dampner_enabled=False):
         no_of_safe_reports += 1
 
+assert no_of_safe_reports == 287, "Result 2,1 is wrong"
 print("Result (2,1):", no_of_safe_reports)
 
+
 # Problem 2
+no_of_safe_reports = 0
+for r in reports:
+    if r.is_safe(dampner_enabled=True):
+        no_of_safe_reports += 1
+
+assert no_of_safe_reports == 354, "Result 2,2 is wrong"
+print("Result (2,2):", no_of_safe_reports)
